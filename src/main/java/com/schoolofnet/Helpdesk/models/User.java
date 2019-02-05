3ae811abd7de737b522e3ff91d25e8cba72eab8b
Eliezer Barbosa <eliezer1789j@gmail.com>
2019-02-05 22:06:48 +0000

    created model Role

--- src/main/java/com/schoolofnet/Helpdesk/models/User.java
+++ src/main/java/com/schoolofnet/Helpdesk/models/User.java
@@ -5,12 +5,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
+@Table(name = "users")
 public class User {
 	
 	@Id
