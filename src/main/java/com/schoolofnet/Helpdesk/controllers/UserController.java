2b939e73a3b519426662599b1ebabb366dd15743
Eliezer Barbosa <eliezer1789j@gmail.com>
2019-02-06 01:13:10 +0000

    created method edit in UserController

--- src/main/java/com/schoolofnet/Helpdesk/controllers/UserController.java
+++ src/main/java/com/schoolofnet/Helpdesk/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.schoolofnet.Helpdesk.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.schoolofnet.Helpdesk.models.User;
@@ -20,6 +21,11 @@ public class UserController {
 	public String create(Model model) {
 		model.addAttribute("user", new User());
 		return "users/create";
+	}
+	
+	@GetMapping("/{id}")
+	public String edit(@PathVariable("id") Long id, Model model) {
+		return "users/edit";
 	}
 
 }
