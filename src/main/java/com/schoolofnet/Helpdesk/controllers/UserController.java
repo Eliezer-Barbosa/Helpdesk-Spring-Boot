775787060d74d15835602b4f521f93c541fe2494
Eliezer Barbosa <eliezer1789j@gmail.com>
2019-02-06 01:07:54 +0000

    created method index in UserController

--- src/main/java/com/schoolofnet/Helpdesk/controllers/UserController.java
+++ src/main/java/com/schoolofnet/Helpdesk/controllers/UserController.java
@@ -1,10 +1,17 @@
 package com.schoolofnet.Helpdesk.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
+	
+	@GetMapping
+	public String index(Model model) {
+		return "users/index";
+	}
 
 }
