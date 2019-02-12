603aa38097e8f7b87947356aac3e1100880ddba9
Eliezer Barbosa <eliezer1789j@gmail.com>
2019-02-12 23:57:10 +0000

    adding annotation @Repository to RoleRepository

--- src/main/java/com/schoolofnet/Helpdesk/repositories/RoleRepository.java
+++ src/main/java/com/schoolofnet/Helpdesk/repositories/RoleRepository.java
@@ -1,5 +1,11 @@
 package com.schoolofnet.Helpdesk.repositories;
 
-public interface RoleRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.schoolofnet.Helpdesk.models.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long>{
 
 }
