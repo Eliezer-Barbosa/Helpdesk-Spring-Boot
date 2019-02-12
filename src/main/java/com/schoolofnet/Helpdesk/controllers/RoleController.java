60a07c11c1a9ce18788768ac714a87f9c457da30
Eliezer Barbosa <eliezer1789j@gmail.com>
2019-02-12 23:33:56 +0000

    created class RoleController

---
+++ src/main/java/com/schoolofnet/Helpdesk/controllers/RoleController.java
@@ -0,0 +1,10 @@
+package com.schoolofnet.Helpdesk.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/roles")
+public class RoleController {
+
+}
