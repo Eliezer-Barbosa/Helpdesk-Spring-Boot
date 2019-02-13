f1414b52a7e8cd80f8d9f6ae3d64c93b15b14277
Eliezer Barbosa <eliezer1789j@gmail.com>
2019-02-13 00:13:36 +0000

    adding dependency injection in RoleServiceImpl class with @Autowired annotation

--- src/main/java/com/schoolofnet/Helpdesk/services/RoleServiceImpl.java
+++ src/main/java/com/schoolofnet/Helpdesk/services/RoleServiceImpl.java
@@ -2,9 +2,19 @@ package com.schoolofnet.Helpdesk.services;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.schoolofnet.Helpdesk.models.Role;
+import com.schoolofnet.Helpdesk.repositories.RoleRepository;
 
 public class RoleServiceImpl implements RoleService {
+	
+	@Autowired
+	private RoleRepository repository;
+	
+	public RoleServiceImpl(RoleRepository repository) {
+		this.repository = repository;
+	}
 
 	@Override
 	public List<Role> findAll() {
