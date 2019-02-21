0e9a605cf80e3086713e692a59407ba8acdfa9ab
Eliezer Barbosa <eliezer1789j@gmail.com>
2019-02-21 20:48:36 +0000

    created UserService class and interface

---
+++ src/main/java/com/schoolofnet/Helpdesk/services/UserServiceImpl.java
@@ -0,0 +1,29 @@
+package com.schoolofnet.Helpdesk.services;
+
+import java.util.List;
+
+import com.schoolofnet.Helpdesk.models.User;
+
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public List<User> findAll() {
+		return null;
+	}
+
+	@Override
+	public User create(User user) {
+		return null;
+	}
+
+	@Override
+	public Boolean delete(Long id) {
+		return null;
+	}
+
+	@Override
+	public Boolean update(Long id, User user) {
+		return null;
+	}
+
+}
