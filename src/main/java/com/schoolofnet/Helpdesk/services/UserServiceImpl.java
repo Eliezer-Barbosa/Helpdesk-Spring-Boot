875b2a105b8d25023a7687acf2854c58c2687cac
Eliezer Barbosa <eliezer1789j@gmail.com>
2019-02-22 10:54:21 +0000

    dependency injection in UserServiceImpl class

--- src/main/java/com/schoolofnet/Helpdesk/services/UserServiceImpl.java
+++ src/main/java/com/schoolofnet/Helpdesk/services/UserServiceImpl.java
@@ -2,9 +2,19 @@ package com.schoolofnet.Helpdesk.services;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.schoolofnet.Helpdesk.models.User;
+import com.schoolofnet.Helpdesk.repositories.UserRepository;
 
 public class UserServiceImpl implements UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public UserServiceImpl(UserRepository repository) {
+		this.repository = repository;
+	}
 
 	@Override
 	public List<User> findAll() {
