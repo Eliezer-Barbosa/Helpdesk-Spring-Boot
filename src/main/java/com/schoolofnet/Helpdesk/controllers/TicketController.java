11489dbb6e17709dded026c170cb12bc47c2479d
Eliezer Barbosa <eliezer1789j@gmail.com>
2019-03-05 14:46:39 +0000

    create.html - tickets form

--- src/main/java/com/schoolofnet/Helpdesk/controllers/TicketController.java
+++ src/main/java/com/schoolofnet/Helpdesk/controllers/TicketController.java
@@ -5,12 +5,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.schoolofnet.Helpdesk.models.Ticket;
+
 @Controller
 @RequestMapping("/tickets")
 public class TicketController {
 	
 	@GetMapping
 	public String create(Model model) {
+		model.addAttribute("ticket",new Ticket());
 		return "ticket/create";
 	}
 
