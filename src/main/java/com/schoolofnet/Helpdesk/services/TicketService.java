fd98f479325fa87ba1aea1df10abac7c9e2750a4
Eliezer Barbosa <eliezer1789j@gmail.com>
2019-03-05 14:25:42 +0000

    ticket service created

---
+++ src/main/java/com/schoolofnet/Helpdesk/services/TicketService.java
@@ -0,0 +1,15 @@
+package com.schoolofnet.Helpdesk.services;
+
+import java.util.List;
+
+import com.schoolofnet.Helpdesk.models.Ticket;
+
+public interface TicketService {
+	
+	public List<Ticket> findAll();
+	public Ticket create(Ticket ticket);
+	public Boolean delete(Long id);
+	public Boolean update(Long id, Ticket ticket);
+	public Ticket show(Long id);
+
+}
---
+++ src/main/java/com/schoolofnet/Helpdesk/services/TicketServiceImpl.java
@@ -0,0 +1,38 @@
+package com.schoolofnet.Helpdesk.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.schoolofnet.Helpdesk.models.Ticket;
+
+@Service
+public class TicketServiceImpl implements TicketService {
+
+	@Override
+	public List<Ticket> findAll() {
+		return null;
+	}
+
+	@Override
+	public Ticket create(Ticket ticket) {
+		return null;
+	}
+
+	@Override
+	public Boolean delete(Long id) {
+		return null;
+	}
+
+	@Override
+	public Boolean update(Long id, Ticket ticket) {
+		return null;
+	}
+
+	@Override
+	public Ticket show(Long id) {
+		return null;
+	}
+	
+
+}
