38b03612489522237dc89d84dbca1b8ce9d2aa43
Eliezer Barbosa <eliezer1789j@gmail.com>
2019-03-05 14:15:41 +0000

    ticket controller created

---
+++ src/main/java/com/schoolofnet/Helpdesk/controllers/TicketController.java
@@ -0,0 +1,17 @@
+package com.schoolofnet.Helpdesk.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/tickets")
+public class TicketController {
+	
+	@GetMapping
+	public String create(Model model) {
+		return "ticket/create";
+	}
+
+}
