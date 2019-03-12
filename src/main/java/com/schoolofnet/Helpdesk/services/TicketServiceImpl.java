23484c0ec11f55da2d7bd0a72e7a6aef82d95aa4
Eliezer Barbosa <eliezer1789j@gmail.com>
2019-03-12 20:00:11 +0000

    making some changes in the front end design

--- src/main/java/com/schoolofnet/Helpdesk/services/TicketServiceImpl.java
+++ src/main/java/com/schoolofnet/Helpdesk/services/TicketServiceImpl.java
@@ -93,7 +93,7 @@ public class TicketServiceImpl implements TicketService {
 
 	@Override
 	public Ticket show(Long id) {
-		return this.ticketRepository.findOne(id);
+		return findById(id);
 	}
 
 	@Override
